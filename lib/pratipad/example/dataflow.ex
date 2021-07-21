54aae3e9f9d9aec8754b4f3600463c7f8659e044
Kentaro Kuribayashi <kentarok@gmail.com>
2021-07-21 23:29:56 +0900

    rename dataflow file

--- lib/pratipad/example/dataflow.ex
+++ lib/pratipad/example/dataflow.ex
@@ -1,6 +1,6 @@
 defmodule Pratipad.Example.Dataflow do
   use Pratipad.Dataflow
-  alias Pratipad.Dataflow.Push
+  alias Pratipad.Dataflow.Demand
   alias Pratipad.Example.Processor.Precipitation
 
   def declare() do
