9860d150679b986f080e99c6d9d63cf84d9cd143
Kentaro Kuribayashi <kentarok@gmail.com>
2021-09-16 10:29:11 +0900

    fix a module name

--- lib/pratipad/example/dataflow.ex
+++ lib/pratipad/example/dataflow.ex
@@ -1,6 +1,6 @@
 defmodule Pratipad.Example.Dataflow do
   use Pratipad.Dataflow
-  alias Pratipad.Dataflow.Demand
+  alias Pratipad.Dataflow.{Demand, Output}
   alias Pratipad.Example.Processor.Precipitation
 
   def declare() do
